ebd5baa7770dd13bfffa11354f82e14f3fbc1277
Denes Nagy <dock3r@gmail.com>
2024-12-22 15:10:55 +0100

    remove leftover css rules

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import JsonLookup from './components/JsonLookup'
 
 function App() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-gray-50">
       <JsonLookup />
     </div>
   )
