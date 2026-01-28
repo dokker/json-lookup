03203982624eb3af818aae2d3ca090a2b3e86c75
Denes Nagy <dock3r@gmail.com>
2026-01-28 11:34:40 +0100

    fix coloring active elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,23 @@ module.exports = {
     './src/**/*.{js,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(var(--primary))",
+        "primary-foreground": "hsl(var(--primary-foreground))",
+        secondary: "hsl(var(--secondary))",
+        "secondary-foreground": "hsl(var(--secondary-foreground))",
+        destructive: "hsl(var(--destructive))",
+        "destructive-foreground": "hsl(var(--destructive-foreground))",
+        accent: "hsl(var(--accent))",
+        "accent-foreground": "hsl(var(--accent-foreground))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        "ring-offset-background": "hsl(var(--ring-offset-background))",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 }
