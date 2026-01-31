7f3d16ff791b6f1bd064b6690c41d5f2fb738fcf
Denes Nagy <dock3r@gmail.com>
2026-01-31 15:09:57 +0100

    add shadows

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -7,9 +7,9 @@ const Button = React.forwardRef(({ className, variant = "default", size = "defau
   return (
     <Comp
       className={cn(
-        "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+        "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium shadow-md ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
         {
-          "bg-primary text-primary-foreground hover:bg-primary/90 active:bg-primary/80": variant === "default",
+          "bg-primary text-primary-foreground shadow-sm hover:bg-primary/90 active:bg-primary/80": variant === "default",
           "bg-destructive text-destructive-foreground hover:bg-destructive/90 active:bg-destructive/80": variant === "destructive",
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground active:bg-accent/80": variant === "outline",
           "bg-secondary text-secondary-foreground hover:bg-secondary/80 active:bg-secondary/70": variant === "secondary",
